e8e5f3262b05a4b6ec7d6d27c8bf33f795f2f20b
Bhavishya Sharma <bhavishyastudy05@gmail.com>
2026-06-23 22:44:17 +0530

    Create unit3_4.c

---
+++ Practice/Course1/unit3_4.c
@@ -0,0 +1,11 @@
+//divide numbers
+# include <stdio.h>
+int main(void){
+    double cel;
+    double fh;
+    scanf("%lf",&cel);
+    fh = (cel*(9.0 / 5.0)) + 32.0;
+    printf("%.1f",fh);
+    return 0;
+}
+
