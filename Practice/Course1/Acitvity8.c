2c233f9de0456e1ce0bcd77183f5de28eda405ce
Bhavishya Sharma <bhavishyastudy05@gmail.com>
2026-06-23 22:22:39 +0530

    Create Acitvity8.c

---
+++ Practice/Course1/Acitvity8.c
@@ -0,0 +1,17 @@
+//put lines in the correct order
+ // Preprocessor directive
+ #include <stdio.h>
+ // main function
+ int main(void){
+// variable declaration
+    int i=0;
+    printf("I ");
+// executable statement
+    for(i = 0; i < 3 ; i++) {
+        printf("love ");
+    }
+     printf("programming!");
+// return statement
+    return(0);
+}
+
