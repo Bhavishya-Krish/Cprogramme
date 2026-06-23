b3e9840d6a28f10fe1d129dc40cc5ae97b72cb09
Bhavishya Sharma <bhavishyastudy05@gmail.com>
2026-06-23 22:41:40 +0530

    Create unit3_1.c

---
+++ Practice/Course1/unit3_1.c
@@ -0,0 +1,13 @@
+//print characters
+# include <stdio.h>//Programming in C
+int main(void){
+    char letter1;
+    letter1 = 'i';
+    char letter2;
+    letter2 = 'n';
+    char letter3;
+    letter3 = 'C';
+    printf("Programming %c%c %c",letter1,letter2,letter3);
+    return 0;
+    
+}
